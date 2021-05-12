b8006c43dee7f2377b6973be4de921fb960e462e
piladzis10 <davis.burmistris@gmail.com>
2021-05-12 11:05:31 +0300

    first commit

---
+++ django/blog_api/tests.py
@@ -0,0 +1,18 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from blog.models import Post, Category
+from django.contrib.auth.models import User
+
+
+class PostTests(APITestCase):
+
+    def test_view_posts(self):
+        """
+        Ensure we can view all objects.
+        """
+        url = reverse('blog_api:listcreate')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    
